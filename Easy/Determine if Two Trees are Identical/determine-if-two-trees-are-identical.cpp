9f268a107458ea1dd51450bfef3b834c83e7a697
Amit Yadav <88823371+Tyrant670@users.noreply.github.com>
2023-11-21 17:17:39 +0530

    Updated Solution - GfG to GitHub

--- Easy/Determine if Two Trees are Identical/determine-if-two-trees-are-identical.cpp
+++ Easy/Determine if Two Trees are Identical/determine-if-two-trees-are-identical.cpp
@@ -42,8 +42,7 @@ class Solution
     bool isIdentical(Node *a, Node *b)
     {
         if(a==NULL and b==NULL) return true;
-        if(a==NULL or b==NULL) return false;
-        if(a->data != b->data) return false;
+        if((a==NULL or b==NULL) or (a->data != b->data)) return false;
         return isIdentical(a->left,b->left) && isIdentical(a->right,b->right);
     }
 };
